e45ff59dbd92315befb21c43fec449100c4605d9
Nathalie Rauschmayr <n.rauschmayr@gmail.com>
2017-02-24 13:38:19 -0800

    Update generate_graph.py

--- postprocessing/generate_graph.py
+++ postprocessing/generate_graph.py
@@ -205,17 +205,6 @@ class Graph(object):
         return output
       else:
         return data 
-
-def select_k(spectrum, minimum_energy = 0.9):
-    running_total = 0.0
-    total = sum(spectrum)
-    if total == 0.0:
-        return len(spectrum)
-    for i in range(len(spectrum)):
-        running_total += spectrum[i]
-        if running_total / total >= minimum_energy:
-            return i + 1
-    return len(spectrum)
    
 if __name__ == "__main__":
    g = Graph(sys.argv[1])
